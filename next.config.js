c10f50271596009bcb45edb8bea5e1c0746c537e
Air2air <todd@dunningmarketing.com>
2024-08-01 12:39:47 -0700

    chore: Update next.config.js with environment variables

--- next.config.js
+++ next.config.js
@@ -9,5 +9,13 @@ module.exports = {
         pathname: '**'
       }
     ]
-  }
+  },
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    KV_REST_API_READ_ONLY_TOKEN: process.env.KV_REST_API_READ_ONLY_TOKEN,
+    KV_REST_API_TOKEN: process.env.KV_REST_API_TOKEN,
+    KV_REST_API_URL: process.env.KV_REST_API_URL,
+    KV_URL: process.env.KV_URL,
+  },
 }
